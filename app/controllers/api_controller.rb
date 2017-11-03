cf66bf00f5e27b512aa6425099918777641d7727
Skiparin <og1806x9@hotmail.com>
2017-11-03 14:57:55 +0100

    added filter for user

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,7 +2,7 @@ class ApiController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery with: :null_session
 
-before_filter :set_default_response_format
+before_action :set_default_response_format
 
 private
   def set_default_response_format
