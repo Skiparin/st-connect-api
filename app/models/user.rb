3b04f1e867ce8f2d3b3d333df537054a35ae8545
Skiparin <og1806x9@hotmail.com>
2017-11-03 11:05:31 +0100

    need to push to server

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
           :omniauthable
+
   include DeviseTokenAuth::Concerns::User
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email,                 allow_nil: true,
+                                    format: { with: VALID_EMAIL_REGEX },
+                                    uniqueness: { case_sensitive: false },
+									uniqueness: true, if: :email
+
 end
