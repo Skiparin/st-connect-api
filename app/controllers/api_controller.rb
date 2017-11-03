7b77044fbb34bd6d19f924f53dc7ed8cd3af84e8
Skiparin <og1806x9@hotmail.com>
2017-11-03 14:56:35 +0100

    added filter for user

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,11 @@
 class ApiController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery with: :null_session
-  skip_before_action :verify_authenticity_token
+
+before_filter :set_default_response_format
+
+private
+  def set_default_response_format
+    request.format = :json
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   skip_before_action :verify_authenticity_token
+
+  before_filter :set_default_response_format
+
+private
+  def set_default_response_format
+    request.format = :json
+  end
 end
