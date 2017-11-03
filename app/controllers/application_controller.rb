5b690c38a883a98c6db18d68333daab9f080ba6a
Skiparin <og1806x9@hotmail.com>
2017-11-03 14:58:37 +0100

    added filter for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   skip_before_action :verify_authenticity_token
 
-  before_filter :set_default_response_format
+  before_action :set_default_response_format
 
 private
   def set_default_response_format
