0485a1dedba0cd59b2c2641a1158edd6113ca557
Skiparin <og1806x9@hotmail.com>
2017-11-09 15:36:05 +0100

    fixed syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,10 @@ class User < ActiveRecord::Base
   include DeviseTokenAuth::Concerns::User
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  
   validates :email,                 allow_nil: true,
                                     format: { with: VALID_EMAIL_REGEX },
                                     uniqueness: { case_sensitive: false },
-									uniqueness: true, if: :email
+									                  uniqueness: true, if: :email
 
 end
