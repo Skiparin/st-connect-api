821bcb026c4b9a1caad2d8d7a1472251c6cbd7ca
skiparin <og1806x9@hotmail.com>
2017-11-13 15:23:06 +0100

    added a admin page for like

---
+++ app/admin/like.rb
@@ -0,0 +1,26 @@
+ActiveAdmin.register Like do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+form do |f|
+    f.inputs 'Like' do
+      f.input :user_id, label: "User", as: :select, collection: User.all.each.map{|t| ["#{t.name}", t.id]}, include_blank: false
+      f.input :post_id, label: "Post", as: :select, collection: Post.all.each.map{|t| ["#{t.id}", t.id]}, include_blank: false
+    end
+    f.semantic_errors *f.object.errors.keys
+    f.actions
+end
+
+permit_params :user_id, :post_id
+
+end
