26790c142174c1378e04e88f5645f039f223edde
Skiparin <og1806x9@hotmail.com>
2018-01-08 17:34:17 +0100

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:show] do
-    resources :profiles, only: [:show]
+  resources :profiles, only: [:show] do
   	resources :user_searches, only: [:index, :create]
   	resources :educations, only: [:create, :update]
   	resources :experiences, only: [:create, :update]
