16fd064a5293e1c84dacfcc71f292c94dcc381da
Skiparin <og1806x9@hotmail.com>
2018-01-10 21:00:38 +0100

    created a method to be used in active admin dashboard

--- app/models/search_statistic.rb
+++ app/models/search_statistic.rb
@@ -17,9 +17,24 @@ class SearchStatistic < ApplicationRecord
 
   has_and_belongs_to_many :profile
 
+  def get_max_ten_pairs
+    job_desc_hash = self.job_descriptions_using_search
+    if job_desc_hash.length <= 5
+      return job_desc_hash
+    end
+    sorted_hash = job_desc_hash.sort.reverse
+    return_hash = {}
+    sorted_hash.each do |s_h| 
+      if return_hash.length == 5
+        break
+      end
+      return_hash[s_h[0]] = s_h[1]
+    end
+    return return_hash
+  end
+
   private
   	def set_number_of_searches_to_one
   		self.number_of_searches = 1
   	end
-
 end
